39c2495582ad6550feedffa771354126eb99d706
r55man <r55man@gmail.com>
2026-03-07 01:07:52 -0500

    kbuild edits

--- cmake/20_targets.cmake
+++ cmake/20_targets.cmake
@@ -9,6 +9,19 @@ if(NOT KCLI_BUILD_STATIC AND NOT KCLI_BUILD_SHARED)
     message(FATAL_ERROR "kcli requires at least one of KCLI_BUILD_STATIC or KCLI_BUILD_SHARED to be ON.")
 endif()
 
+function(ktools_apply_runtime_rpath target_name)
+    if(NOT TARGET "${target_name}")
+        return()
+    endif()
+    if(NOT DEFINED KTOOLS_RUNTIME_RPATH_DIRS OR KTOOLS_RUNTIME_RPATH_DIRS STREQUAL "")
+        return()
+    endif()
+    set_target_properties("${target_name}" PROPERTIES
+        BUILD_RPATH "${KTOOLS_RUNTIME_RPATH_DIRS}"
+        INSTALL_RPATH "${KTOOLS_RUNTIME_RPATH_DIRS}"
+    )
+endfunction()
+
 if(KCLI_BUILD_STATIC)
     add_library(kcli_sdk_static STATIC ${KCLI_SOURCES})
     add_library(kcli::sdk_static ALIAS kcli_sdk_static)
@@ -43,6 +56,7 @@ if(KCLI_BUILD_SHARED)
         OUTPUT_NAME kcli
         EXPORT_NAME sdk_shared
     )
+    ktools_apply_runtime_rpath(kcli_sdk_shared)
 endif()
 
 if(TARGET kcli_sdk_shared)
